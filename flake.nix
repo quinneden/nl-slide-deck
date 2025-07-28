2e0f620e921adf3cf2dcb9c15566e3586c85693a
Quinn Edenfield <quinn@qeden.dev>
2025-07-28 00:21:04 -0700

    nix nix

--- flake.nix
+++ flake.nix
@@ -6,37 +6,18 @@
   };
 
   outputs =
-    { nixpkgs, self }:
+    { nixpkgs, ... }:
     let
-      inherit (nixpkgs) lib;
-
-      systems = [
-        "x86_64-linux"
-        "aarch64-darwin"
-      ];
-
-      forEachSystem = f: lib.genAttrs systems (system: f { pkgs = import nixpkgs { inherit system; }; });
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; };
     in
     {
-      packages = forEachSystem (
-        { pkgs }:
-        {
-          default = self.packages.${pkgs.system}.slide-deck;
-          slide-deck = pkgs.callPackage ./package.nix { };
-        }
-      );
-
-      devShells = forEachSystem (
-        { pkgs }:
-        {
-          default = pkgs.mkShell {
-            name = "slide-deck";
-            packages = with pkgs; [
-              pnpm
-              nodejs
-            ];
-          };
-        }
-      );
+      devShells.aarch64-darwin.default = pkgs.mkShell {
+        name = "slide-deck";
+        packages = with pkgs; [
+          pnpm
+          nodejs
+        ];
+      };
     };
 }
